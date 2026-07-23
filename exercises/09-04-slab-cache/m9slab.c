de66a44b7bc73814810d07e6739540967d07aabc
eirueimi <eirueimi@gmail.com>
2026-07-23 12:40:52 +0900

    exercise: 09-04-slab-cache

---
+++ exercises/09-04-slab-cache/m9slab.c
@@ -0,0 +1,27 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+
+MODULE_LICENSE("GPL");
+
+static struct kmem_cache *m9_cache;
+static void *m9_obj;
+
+static int __init m9slab_init(void)
+{
+	/* TODO: kmem_cache_create("m9cache", 128, 0, 0, NULL) でキャッシュを作り、
+	 * kmem_cache_alloc() でオブジェクトを1個確保してから
+	 * "m9slab: created and allocated" を pr_info せよ */
+	return 0;
+}
+static void __exit m9slab_exit(void)
+{
+	if (m9_obj)
+		kmem_cache_free(m9_cache, m9_obj);
+	if (m9_cache)
+		kmem_cache_destroy(m9_cache);
+}
+
+module_init(m9slab_init);
+module_exit(m9slab_exit);
